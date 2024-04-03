11f74d8b99496612940382244468a315c2fd9468
srikargridpro <147362350+srikargridpro@users.noreply.github.com>
2024-04-03 17:26:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "hash_map_128_bit.h"
 #include <iostream>
+#include "shared_ptr"
 int main() {
     // Example usage
     HashMap<int, std::string , 20> hashmap;
@@ -24,6 +25,32 @@ int main() {
     }
     std::cout << "iter count = " << count << std::endl;
     std::cout << "Total size = " << hashmap.getTotalSize() << std::endl;
+    
+    // Shared Ptr test
+    gp::atomic<int> atomic_obj(10);
+    atomic_obj = 19;
+    ++atomic_obj;
+    atomic_obj == 20 ? std::cout << "Atomic object is 20\n" : std::cout << "Atomic object is not 20\n";
+    atomic_obj += 20;
+    gp::atomic<char> (10);
+    gp::semi_atomic<int> semi_atomic_obj(10);
+    atomic_obj = semi_atomic_obj;
 
+    std::vector<int> vec = {1, 2, 3, 4, 5};
+    for(auto& val : vec)
+    {
+        std::cout << val << " ";
+    }
+
+    gp::shared_ptr<std::vector<int>> shared_ptr_obj((vec));
+    std::cout << "After shared pointer\n";
+    for(auto& val : vec)
+    {
+        std::cout << val << " ";
+    }
+    
+    // reclaimer<std::vector<int>>::reclaim();
+
+}
     return 0;
 }
